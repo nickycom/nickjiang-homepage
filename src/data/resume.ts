ca863f84125ca8149da29fea80dfaf746d6b9327
Nick Jiang <nickycom@msn.com>
2026-09-16 22:48:21 +0800

    sync: update resume data (2026-09-16 v2)

--- src/data/resume.ts
+++ src/data/resume.ts
@@ -67,8 +67,8 @@ const data: Record<"zh" | "en", ResumeData> = {
             "主导国际/国内品牌主题战役，驱动地方品牌焕新与高端化。",
             "主导喜力高端化，借潮汕强势市场新年本土文化植入与夏季整合战役，实现销量双位数增长。",
             "主导雪花勇闯天涯大单品年度沟通策略，切入电竞场景，规模化招募年轻人群。",
-            "通过老雪品牌和金威区域品牌创新产品，提升国内品牌产品吨酒价格3%，",
-            "主导渠道费用优化及其渠道数字化建设",
+            "通过老雪品牌和金威区域品牌创新产品，提升国内品牌产品吨酒价格3%。",
+            "主导渠道费用优化及其渠道数字化建设。",
             "现饮渠道：优化促销员投入，推行终端服务员数字化激励，中高端及高端覆盖提升 8%，核心市场单店动销提升 15%，打造全国数字化标杆市场。",
             "非现饮渠道：通过推动数字化消费者激励实现综合终端覆盖提升 15%，动销增长 8%。"
           ],
@@ -91,9 +91,9 @@ const data: Record<"zh" | "en", ResumeData> = {
           bullets: [
             "主导哈啤品牌老牌焕新与全产品线增长，中高端市场份额 4 年间提升一个百分点。",
             "通过“产品重塑+年轻化内容生成+数字化媒体渗透+加强渠道营销”组合拳，实现3年间大单品哈冰纯销量扭转。",
-            "主导中高端女性市场开发以及哈尔滨创新产品（哈尔滨白啤）端到端上市，贡献哈啤中高端核心市场 10% 增长。",
+            "主导中高端女性市场开发以及哈尔滨创新产品（哈尔滨白啤）端到端上市，贡献哈啤中高端核心市场10% 增长。",
             "主导高端经典型拉格新品开发上市，抢占居家饮用场景消费趋势，销量全年达成率 120%。",
-            "启动品牌年轻化再定位，年轻化品牌力 4 年增长 15%，成为中国年轻消费者最喜爱啤酒品牌；首创“街头啤酒+街头美食+街头文化”定位，打通体验生态，获 2020 金投赏创意银奖（RC-W-2020-19152）。",
+            "启动品牌年轻化再定位，年轻化品牌力 4 年增长 15%，成为中国年轻消费者最喜爱啤酒品牌；首创“街头啤酒+街头美食+街头文化”差异化定位，建立标志性饮用场景，该案例获 2020 金投赏创意银奖（RC-W-2020-19152）。",
             "后疫情期间联动地方政府与 IP，抢占夜市街头饮用场景；抖音百大美食 KOL 探店直播 + 5000+ 渠道门店联动，渠道营业额提升 60%，声量 4.9 亿曝光、其中35%为免费声量。"
           ],
         },
@@ -152,8 +152,8 @@ const data: Record<"zh" | "en", ResumeData> = {
           bullets: [
             "Drove 13% sales-volume CAGR for the imported beer brand in China, providing strategic consulting, on-site guidance and network access.",
             "Optimized the portfolio and established a strategy focused on the premium classic-lager segment.",
-            "Lead brand localization and repositioning, launched digital content marketing, and connected offline 	activation with digital marketing.",
-            "Drove peak-season marketing campaigns, optimized online/offline resource allocation, built benchmark  markets, and empowered channel customer development."
+            "Lead brand localization and repositioning, launched digital content marketing, and connected offline activation with digital marketing.",
+            "Drove peak-season marketing campaigns, optimized online/offline resource allocation, built benchmark markets, and empowered channel customer development."
           ],
         },
         {
@@ -188,7 +188,7 @@ const data: Record<"zh" | "en", ResumeData> = {
             "Established NOWWA Coffee's brand positioning, portfolio & communication strategy.",
             "Established the \"Fresh-brewed, Ready On-the-go\" positioning and upgraded the youthful visual identity across online content and channels.",
             "Created the \"quality differentiation + coffee cross-over\" new-product strategy, driving 1000% GMV growth in lower-tier markets.",
-            "Created the fruit-coffee sub-category, collaborating with 30+ brands to generate 300M+ impressions; crossed over with lululemon, Keep and SuperMonkey to build lifesytle occasions, lifting Ele.me/Meituan channel growth by 155%."
+            "Created the fruit-coffee sub-category, collaborating with 30+ brands to generate 300M+ impressions; crossed over with lululemon, Keep and SuperMonkey to build lifestyle occasions, lifting Ele.me/Meituan channel growth by 155%."
           ],
         },
         {
@@ -199,9 +199,9 @@ const data: Record<"zh" | "en", ResumeData> = {
             "Led Harbin Beer's brand rejuvenation and full-portfolio growth, lifting Core+ segment market share by 1pt over 4 years.",
             "Turned around Hero SKU (Habin-ICE) within 3 years through product renovation, youth content generation, digital media penetration and trade marketing capacity development.",
             "Led female market development and Harbin innovation (White Beer) end to end launch, contributing 10% growth in Harbin's Core+ segment.",
-            "Led the premium classic lager new-product launch and captured the at-home occasion trend during COVID-19, Exceeding the target by 20%.",
-            "Led brand youth repositioning, lifting youthful brand power by 15% over 4 years and becoming the most-loved beer brand among young Chinese consumers; pioneered the industry-first \"Street Beer + Street Food + Street Culture\" positioning and won the 2020 ROI Creative Festival Silver Award (RC-W-2020-19152).",
-            "Post-pandemic, partnered with local governments and IPs to capture the night-market street occasion; via 100 Douyin food KOL store-visit live-streams and 5,000+ outlet activations, lifted channel revenue by 60%,  generated 490M media impressions and 35% earned buzz."
+            "Led the premium classic lager new-product launch and captured the at-home occasion trend during COVID-19, exceeding the target by 20%.",
+            "Led brand youth repositioning, lifting youthful brand power by 15% over 4 years and becoming the most-loved beer brand among young Chinese consumers; pioneered the industry-first \"Street Beer + Street Food + Street Culture\" differentiated positioning while owned signature drinking occasion for brand, which won the 2020 ROI Creative Festival Silver Award (RC-W-2020-19152).",
+            "Post-COVID-19, partnered with local governments and IPs to capture the night-market street occasion; via 100 Douyin food KOL store-visit live-streams and 5,000+ outlet activations, lifted channel revenue by 60%, generated 490M media impressions and 35% earned buzz."
           ],
         },
         {
